acde9d857f60ef0b6da4e5c2bdd686ba6fde5b1f
ITforAdults <ttymi.kamolov@gmail.com>
2022-05-10 15:18:25 +0200

    fixed noteAdapter

--- app/src/main/java/uz/gita/noteapp/presentation/ui/adapter/NoteAdapter.kt
+++ app/src/main/java/uz/gita/noteapp/presentation/ui/adapter/NoteAdapter.kt
@@ -2,14 +2,20 @@ package uz.gita.noteapp.presentation.ui.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import uz.gita.noteapp.R
 import uz.gita.noteapp.data.model.common.NoteData
 import uz.gita.noteapp.databinding.ItemNoteBinding
+import uz.gita.noteapp.databinding.NoteItemBinding
 
-class NoteAdapter() : ListAdapter<NoteData, NoteAdapter.NoteViewHolder>(NoteDiffUtil) {
+class NoteAdapter : ListAdapter<NoteData, NoteAdapter.NoteViewHolder>(NoteDiffUtil) {
 
     private var noteListener: ((NoteData) -> Unit)? = null
 
@@ -18,20 +24,23 @@ class NoteAdapter() : ListAdapter<NoteData, NoteAdapter.NoteViewHolder>(NoteDiff
         override fun areContentsTheSame(oldItem: NoteData, newItem: NoteData): Boolean = oldItem == newItem
     }
 
-    inner class NoteViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
-
+    inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var image: ImageView
+        var text: TextView
         init {
-            binding.noteLinear.setOnClickListener { noteListener?.invoke(currentList[absoluteAdapterPosition]) }
+            image = view.findViewById(R.id.image)
+            text = view.findViewById(R.id.textView)
+            view.findViewById<LinearLayout>(R.id.note_linear).setOnClickListener { noteListener?.invoke(currentList[absoluteAdapterPosition]) }
         }
 
         fun bind() {
-            binding.noteText.text = currentList[absoluteAdapterPosition].title
+            text.text = currentList[absoluteAdapterPosition].title
             Log.d("TTT", "bind")
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder =
-        NoteViewHolder(ItemNoteBinding.inflate(LayoutInflater.from(parent.context)))
+        NoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false))
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         holder.bind()
